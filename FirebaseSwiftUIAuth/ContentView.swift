af4a8759b2096ee2f495d5bc80148e0d19c489f6
Matthew Dolan <matthew@rocketplantech.com>
2023-04-13 05:40:36 -0700

    Initial Commit

---
+++ FirebaseSwiftUIAuth/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FirebaseSwiftUIAuth
+//
+//  Created by Matthew Dolan on 2023-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseSwiftUIAuth/FirebaseSwiftUIAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseSwiftUIAuthApp.swift
+//  FirebaseSwiftUIAuth
+//
+//  Created by Matthew Dolan on 2023-04-13.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseSwiftUIAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
