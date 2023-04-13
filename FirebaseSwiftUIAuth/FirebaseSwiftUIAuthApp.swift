70a935ec85af5e9f3c65f52ab52d7d4e60fc4f62
Matthew Dolan <matthew@rocketplantech.com>
2023-04-13 06:32:10 -0700

    Made class final.

--- FirebaseSwiftUIAuth/FirebaseSwiftUIAuthApp.swift
+++ FirebaseSwiftUIAuth/FirebaseSwiftUIAuthApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
+final class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
       FirebaseApp.configure()
