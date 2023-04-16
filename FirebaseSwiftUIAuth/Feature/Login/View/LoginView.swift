9b5f6c3af02f4ec77d4ac27af19db7c848c7271a
Matthew Dolan <matthew@rocketplantech.com>
2023-04-16 06:56:56 -0700

    Added input textfields and buttons to Login view, and created forgot password button.

--- FirebaseSwiftUIAuth/Feature/Login/View/LoginView.swift
+++ FirebaseSwiftUIAuth/Feature/Login/View/LoginView.swift
@@ -9,12 +9,53 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            VStack(spacing: 16) {
+                InputTextFieldView(text: .constant(""),
+                                   placeholder: "Email",
+                                   keyboardType: .emailAddress,
+                                   sfSymbol: "envelope")
+                
+                InputTextFieldView(text: .constant(""),
+                                   placeholder: "Password",
+                                   keyboardType: .emailAddress,
+                                   sfSymbol: "locke")
+            }
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    // TODO: Handle presentation to forgot password
+                }, label: {
+                    Text("Forgot Password?")
+                })
+                .font(.system(size: 16,
+                              weight: .bold))
+            }
+            
+            VStack(spacing: 16) {
+                ButtonView(title: "Login") {
+                   // TODO: Handle login action here
+                }
+                
+                ButtonView(title: "Register",
+                           background: .clear,
+                           foreground: .blue,
+                           border: .blue) {
+                    // TODO: Handle presentation to registration
+                }
+            }
+            
+        }
+        .padding(.horizontal, 15)
+        .navigationTitle("Login")
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        NavigationView {
+            LoginView()
+        }
     }
 }
