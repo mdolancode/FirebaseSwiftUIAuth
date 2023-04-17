8c867fcdf67e4c94a0404603d572c6360fe81e96
Matthew Dolan <matthew@rocketplantech.com>
2023-04-17 06:24:45 -0700

    Created Registration Details model.

---
+++ FirebaseSwiftUIAuth/Feature/Register/Model/RegistrationDetails.swift
@@ -0,0 +1,16 @@
+//
+//  RegistrationDetails.swift
+//  FirebaseSwiftUIAuth
+//
+//  Created by Matthew Dolan on 2023-04-17.
+//
+
+import Foundation
+
+struct RegistrationDetails {
+    var email: String
+    var password: String
+    var firstName: String
+    var lastName: String
+    var occupation: String
+}
