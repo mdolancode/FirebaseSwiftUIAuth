df2a4430b47854f89e8706f174450b5beb5bdc82
Matthew Dolan <matthew@rocketplantech.com>
2023-04-17 09:26:14 -0700

    Created view model to get get the values, and if successful store them but if fails throw an error.

---
+++ FirebaseSwiftUIAuth/Feature/Register/View Model/RegistrationViewModel.swift
@@ -0,0 +1,58 @@
+//
+//  RegistrationViewModel.swift
+//  FirebaseSwiftUIAuth
+//
+//  Created by Matthew Dolan on 2023-04-17.
+//
+
+import Combine
+import Foundation
+
+// 1.
+enum RegistrationState {
+    case successful
+    case failed(error: Error)
+    case na
+}
+
+// 2.
+protocol RegistrationViewModel {
+    func register()
+    var service: RegistrationService { get }
+    var state: RegistrationState { get }
+    var userDetails: RegistrationDetails { get }
+    
+    init(service: RegistrationService)
+}
+
+// 3.
+final class RegistrationViewModelImpl: RegistrationViewModel {
+    let service: RegistrationService
+    var state: RegistrationState = .na
+    // 4. Created computed property inside RegistrationDetails
+    var userDetails: RegistrationDetails = RegistrationDetails.new
+    
+    // 5.
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init(service: RegistrationService) {
+        self.service = service
+    }
+    
+    // 5.
+    func register() {
+        service
+            .register(with: userDetails)
+            .sink { [weak self] res in
+                switch res {
+                    case .failure(let error):
+                        self?.state = .failed(error: error)
+                    default: break
+                }
+            } receiveValue: { [weak self] in
+                self?.state = .successful
+            }
+            .store(in: &subscriptions)
+        
+    }
+}
