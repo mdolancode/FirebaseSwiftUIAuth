484da710c4f06bf8c957a91409c574fdbfc08487
Matthew Dolan <matthew@rocketplantech.com>
2023-04-18 06:27:55 -0700

    Created session group and session user details to push out user details to the session the user is currently on.

---
+++ FirebaseSwiftUIAuth/Feature/Session/Model/SessionUserDetails.swift
@@ -0,0 +1,14 @@
+//
+//  SessionUserDetails.swift
+//  FirebaseSwiftUIAuth
+//
+//  Created by Matthew Dolan on 2023-04-18.
+//
+
+import Foundation
+
+struct SessionUserDetails {
+    let firstName: String
+    let lastName: String
+    let occupation: String
+}
